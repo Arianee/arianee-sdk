e9196595233cc5afe4b46f908189e3bbf530b042
alexishecf <alexis@hecfeuille.fr>
2023-04-27 11:32:46 +0200

    feat: add blockchainEvent type and bump

---
+++ packages/protocol-types/src/lib/blockchainEvent.ts
@@ -0,0 +1,19 @@
+export interface BlockchainEvent {
+  returnValues: {
+    [key: string]: unknown;
+  };
+  raw: {
+    data: string;
+    topics: string[];
+  };
+  event: string;
+  signature: string;
+  logIndex: number;
+  transactionIndex: number;
+  transactionHash: string;
+  blockHash: string;
+  blockNumber: number;
+  address: string;
+  chainId: number;
+  network: string;
+}
