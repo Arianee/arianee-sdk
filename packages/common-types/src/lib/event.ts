ce694505f841b2dcfb344a7044acf4f51bf57779
alexishecf <90714480+alexishecf@users.noreply.github.com>
2023-04-28 14:54:13 +0200

    feat(types): add Event interface (#12)

---
+++ packages/common-types/src/lib/event.ts
@@ -0,0 +1,15 @@
+import { ArianeeEventI18N } from './arianeeEvent-i18n';
+import { Protocol } from './protocol';
+
+// ArianeeEvent
+export interface Event {
+  id: string;
+  certificateId: string;
+  pending: boolean;
+  sender: string;
+  timestamp: number;
+  content: ArianeeEventI18N;
+  rawContent: ArianeeEventI18N;
+  imprint: string;
+  protocol: Protocol;
+}
