29d2be34c9436fb431857d629d781b8b7053935c
0xWhiteleaf <noe.comte@outlook.fr>
2024-08-22 16:05:35 +0200

    feat: getSignatureValues

---
+++ packages/utils/src/lib/signature/getSignatureValues.spec.ts
@@ -0,0 +1,21 @@
+import { Wallet, hashMessage } from 'ethers';
+import { getSignatureValues } from './getSignatureValues';
+
+describe('getSignatureValues', () => {
+  it('should throw if the signature is invalid', () => {
+    expect(() => getSignatureValues('invalid')).toThrowError(
+      'Invalid signature'
+    );
+  });
+
+  it('should return the signature values', async () => {
+    const wallet = Wallet.createRandom();
+    const message = 'test';
+    const signature = await wallet.signMessage(message);
+    const result = getSignatureValues(signature);
+
+    const { r, s, v } = wallet.signingKey.sign(hashMessage(message));
+
+    expect(result).toEqual({ r, s, v });
+  });
+});
