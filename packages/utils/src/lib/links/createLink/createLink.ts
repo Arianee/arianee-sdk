a4ec95b2c7ed364df8513d477a19644b82a7e2be
alexishecf <90714480+alexishecf@users.noreply.github.com>
2024-12-19 16:12:46 +0100

    feat: add createResolverLink in utils

--- packages/utils/src/lib/links/createLink/createLink.ts
+++ packages/utils/src/lib/links/createLink/createLink.ts
@@ -1,7 +1,10 @@
 import { BrandIdentity } from '@arianee/common-types';
+import { ethers } from 'ethers';
 
 import { isProtocolV2FromSlug } from '../../slug/slug';
 import { getHostnameFromProtocolName } from '../readLink/readLink';
+
+const RESOLVER_BASE_URL = 'https://q.arianee.net';
 
 /**
  * Creates a valid proof / request / view link for a smart asset based on the protocol slug
@@ -9,6 +12,7 @@ import { getHostnameFromProtocolName } from '../readLink/readLink';
  * @param suffix the optional suffix of the link (e.g. /proof)
  * @param tokenId the token id of the smart asset
  * @param passphrase the passphrase of the smart asset
+ * @param brandIdentity a brand identity object to extract custom domain from (if any), custom domain of the identity will be used as the hostname
  * @returns a valid link
  */
 export const createLink = ({
@@ -36,6 +40,47 @@ export const createLink = ({
       customDomain ?? getHostnameFromProtocolName(slug)
     }${suffix}/${tokenId},${passphrase}`;
   }
+};
+
+/**
+ * Creates a valid proof / request / view link for the link resolver
+ * @param slug the protocol slug (also known as protocol name or network name)
+ * @param suffix the optional suffix of the link (e.g. /proof)
+ * @param tokenId the token id of the smart asset
+ * @param passphrase the passphrase of the smart asset
+ * @param identityAddress the address of the identity that will be used as parameter in the link
+ * @param resolverBaseURL optionally overrides the base url of the link resolver
+ * @returns a valid link
+ */
+export const createResolverLink = ({
+  slug,
+  suffix = '',
+  tokenId,
+  passphrase,
+  identityAddress,
+  resolverBaseURL,
+}: {
+  slug: string;
+  suffix?: string;
+  tokenId: string;
+  passphrase: string;
+  identityAddress: string;
+  resolverBaseURL?: string;
+}) => {
+  const _resolverBaseURL = resolverBaseURL?.endsWith('/')
+    ? resolverBaseURL.slice(0, -1)
+    : resolverBaseURL;
+
+  if (!ethers.isAddress(identityAddress))
+    throw new Error('invalid identity address');
+
+  if (!tokenId) throw new Error('tokenId is required');
+  if (!passphrase) throw new Error('passphrase is required');
+  if (!slug) throw new Error('slug is required');
+
+  return `${
+    _resolverBaseURL ?? RESOLVER_BASE_URL
+  }${suffix}/${tokenId},${passphrase},${slug},${identityAddress}`;
 };
 
 /**
