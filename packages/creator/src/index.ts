1bb37e2fb3da14d438e27325c9e3ad9f3482d520
stefdelec <s.delecroix@gmail.com>
2025-07-29 11:26:56 +0200

    feat(creator): export utils from creator (#361)

--- packages/creator/src/index.ts
+++ packages/creator/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/creator';
 export * from './lib/errors';
 export * from './lib/types';
+export * from './lib/utils/utils';
 import { Creator } from './lib/creator';
 export default Creator;
